27207490d32954c29e31e8f46eef5f23cc50c980
Sagar Khatri <sagark1510@gmail.com>
2020-01-28 12:32:28 +0530

    portrait video rotation issue fixed

--- ios/VideoWatermark.m
+++ ios/VideoWatermark.m
@@ -15,21 +15,22 @@ RCT_EXPORT_METHOD(convert:(NSString *)videoUri imageUri:(nonnull NSString *)imag
     
     AVURLAsset* videoAsset = [[AVURLAsset alloc]initWithURL:[NSURL fileURLWithPath:videoUri] options:nil];
     AVMutableComposition* mixComposition = [AVMutableComposition composition];
-
+    
     AVMutableCompositionTrack *compositionVideoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
     AVAssetTrack *clipVideoTrack = [[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
     AVMutableCompositionTrack *compositionAudioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
     AVAssetTrack *clipAudioTrack = [[videoAsset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0];
     //If you need audio as well add the Asset Track for audio here
-
+    
     [compositionVideoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAsset.duration) ofTrack:clipVideoTrack atTime:kCMTimeZero error:nil];
     [compositionAudioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoAsset.duration) ofTrack:clipAudioTrack atTime:kCMTimeZero error:nil];
-
-    [compositionVideoTrack setPreferredTransform:[[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] preferredTransform]];
     
-    CGSize sizeOfVideo = [[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] naturalSize];
-
-    //Image of watermark 
+    [compositionVideoTrack setPreferredTransform:[[[videoAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] preferredTransform]];
+        
+    CGSize sizeOfVideo = CGSizeApplyAffineTransform(clipVideoTrack.naturalSize, clipVideoTrack.preferredTransform);
+    sizeOfVideo.width = fabs(sizeOfVideo.width);
+    
+    //Image of watermark
     UIImage *myImage=[UIImage imageWithContentsOfFile:imageUri];
     
     UIGraphicsBeginImageContext(sizeOfVideo);
@@ -42,57 +43,70 @@ RCT_EXPORT_METHOD(convert:(NSString *)videoUri imageUri:(nonnull NSString *)imag
     CALayer *layerCa = [CALayer layer];
     layerCa.contents = (id)myImage.CGImage;
     layerCa.frame = CGRectMake(0, 0, myImage.size.width, myImage.size.height);
-    layerCa.opacity = 1.0; 
-
+    layerCa.opacity = 1.0;
+    
     CALayer *parentLayer=[CALayer layer];
     CALayer *videoLayer=[CALayer layer];
     parentLayer.frame=CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
     videoLayer.frame=CGRectMake(0, 0, sizeOfVideo.width, sizeOfVideo.height);
     [parentLayer addSublayer:videoLayer];
     [parentLayer addSublayer:layerCa];
-
+    
     AVMutableVideoComposition *videoComposition=[AVMutableVideoComposition videoComposition] ;
     videoComposition.frameDuration=CMTimeMake(1, 30);
     videoComposition.renderSize=sizeOfVideo;
     videoComposition.animationTool=[AVVideoCompositionCoreAnimationTool videoCompositionCoreAnimationToolWithPostProcessingAsVideoLayer:videoLayer inLayer:parentLayer];
-
+    
     AVMutableVideoCompositionInstruction *instruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
     instruction.timeRange = CMTimeRangeMake(kCMTimeZero, [mixComposition duration]);
     AVAssetTrack *videoTrack = [[mixComposition tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
     
     AVMutableVideoCompositionLayerInstruction* layerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
     
-//    CGAffineTransform t1 = [compositionVideoTrack preferredTransform];
-//    [layerInstruction setTransform:t1 atTime:kCMTimeZero];
+    BOOL  isAssetPortrait_  = NO;
+    CGAffineTransform trackTransform = clipVideoTrack.preferredTransform;
+    if(trackTransform.a == 0 && trackTransform.b == 1.0 && trackTransform.c == -1.0 && trackTransform.d == 0)  {
+        isAssetPortrait_ = YES;
+    }
+    if(trackTransform.a == 0 && trackTransform.b == -1.0 && trackTransform.c == 1.0 && trackTransform.d == 0)  {
+        isAssetPortrait_ = YES;
+    }
+    CGFloat assetScaleToFitRatio = 320.0/clipVideoTrack.naturalSize.width;
+    if(isAssetPortrait_){
+        assetScaleToFitRatio = 320.0/clipVideoTrack.naturalSize.height;
+        CGAffineTransform assetScaleFactor = CGAffineTransformMakeScale(assetScaleToFitRatio,assetScaleToFitRatio);
+        [layerInstruction setTransform:CGAffineTransformConcat(clipVideoTrack.preferredTransform, assetScaleFactor) atTime:kCMTimeZero];
+    }
+    
     
     instruction.layerInstructions = [NSArray arrayWithObject:layerInstruction];
     videoComposition.instructions = [NSArray arrayWithObject: instruction];
-
+    
     NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)objectAtIndex:0];
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setDateFormat:@"yyyy-MM-dd_HH-mm-ss"];
     NSString *destinationPath = [documentsDirectory stringByAppendingFormat:@"/output_%@.mov", [dateFormatter stringFromDate:[NSDate date]]];
-
-    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetMediumQuality];
+    
+    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetHighestQuality];
     exportSession.videoComposition=videoComposition;
-
+    
     exportSession.outputURL = [NSURL fileURLWithPath:destinationPath];
     exportSession.outputFileType = AVFileTypeQuickTimeMovie;
     [exportSession exportAsynchronouslyWithCompletionHandler:^{
-    switch (exportSession.status)
-    {
-        case AVAssetExportSessionStatusCompleted:
-            NSLog(@"Export OK");
-            callback(@[destinationPath]);
-            break;
-        case AVAssetExportSessionStatusFailed:
-            NSLog (@"AVAssetExportSessionStatusFailed: %@", exportSession.error);
-            break;
-        case AVAssetExportSessionStatusCancelled:
-            NSLog(@"Export Cancelled");
-            break;
-    }
-  }];
+        switch (exportSession.status)
+        {
+            case AVAssetExportSessionStatusCompleted:
+                NSLog(@"Export OK");
+                callback(@[destinationPath]);
+                break;
+            case AVAssetExportSessionStatusFailed:
+                NSLog (@"AVAssetExportSessionStatusFailed: %@", exportSession.error);
+                break;
+            case AVAssetExportSessionStatusCancelled:
+                NSLog(@"Export Cancelled");
+                break;
+        }
+    }];
 }
 
 @end
